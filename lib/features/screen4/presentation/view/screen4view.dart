52f816b98b4a326377133201857d40ca06fd3414
hosamff2001 <hosamff2001@gmail.com>
2023-09-04 23:41:19 +0300

    finish screen four

--- lib/features/screen4/presentation/view/screen4view.dart
+++ lib/features/screen4/presentation/view/screen4view.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/intro_wear_suit.dart';
+import 'widgets/take_off_suit.dart';
+import 'widgets/tie_belt.dart';
+import 'widgets/wear_jacket.dart';
+import 'widgets/wear_pants.dart';
+import 'widgets/wear_suit.dart';
+
+
 class Screen4View extends StatelessWidget {
   const Screen4View({super.key});
 
@@ -7,7 +15,31 @@ class Screen4View extends StatelessWidget {
   Widget build(BuildContext context) {
     return const SafeArea(
         child: Scaffold(
-      body: null,
-    ));
+            body: SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              IntroofWearJudoSuit(),
+              WearPants(),
+              WearJacket(),
+              TieBelt(),
+              TakeOffSuit(),
+              WearSuit(),
+            ],
+          ),
+        ),
+      ),
+    )));
   }
 }
+
+
+
+
+
+
+
